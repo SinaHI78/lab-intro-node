c652f4bf4e5a92a5055ce8203839b0051e6cb9af
Sina Joos <sinajoos@gmx.de>
2022-03-23 21:12:03 +0100

    first iteration done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.length++;
+    for (let i = 0; i < this.items.length; i++) {
+      if (item < this.items[i]) {
+        this.items.splice(i, 0, item);
+        return;
+      }
+    }
+    this.items.push(item);
+  }
 
   get(pos) {}
 
@@ -15,3 +27,5 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+const list = new SortedList();
